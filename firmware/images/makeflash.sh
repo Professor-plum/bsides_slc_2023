3d26cb9d589a3ba10436e2687d65649e4305afc7
Profressor Plum <waylon.grange@gmail.com>
2023-01-16 17:55:57 -0700

    Updated images

--- firmware/images/makeflash.sh
+++ firmware/images/makeflash.sh
@@ -2,11 +2,13 @@
 
 rm flash.bin
 
-for i in *.bmp
+for i in *.png
 do
-  convert $i -colorspace gray +matte -colors 2 -depth 1 temp.gray
+  convert $i -flop -colorspace gray +matte -colors 2 -depth 1 temp.gray
   truncate -s +80 temp.gray
   cat temp.gray >> flash.bin
 done
+
+rm temp.gray
 
 truncate -s 1M flash.bin
